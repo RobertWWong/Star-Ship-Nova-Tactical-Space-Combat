70355d420436a4856e8846dad55a48f4162da41f
archon263 <archon263@gmail.com>
2017-05-13 11:47:18 -0700

    added more properties

---
+++ Entities/Ship.java
@@ -0,0 +1,13 @@
+public class Ship {
+	private String name;
+	private int hp;		//Hull point
+	private int storageSpace;	//structural limitation to consider in ship design. We'll leave this as "all the things that makes the ship shoot, move, and enhance its ability to survive"
+	private Object shipDesign; 	//corvette, frigate, cruisers, BB, etc
+	private Object capacitors;	//will be a class of its own, consider this your active battery for life sustaining operations
+	private Object shieldType;	//the secret to life is to be varied
+	private Object hardPoint;	//amount of weapon specific mounts you can have on this ship. May or may not be a separate class
+	private Object engines;		//need some sort of propulsion system
+
+	
+	
+}
